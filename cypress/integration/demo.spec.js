fad4eed7a1f7e34b626d5b06d798aa7eebd7d87a
Philip Griffin <philipgriffin88@gmail.com>
2022-04-16 15:51:40 +0100

    chore: remove unused files and clean-up baseUrl

--- cypress/integration/demo.spec.js
+++ cypress/integration/demo.spec.js
@@ -1,22 +1,21 @@
-describe('Demo Testing', () => {
-    it('can open a blog page', () => {
-        cy.visit('http://localhost:1313/');
+describe("Demo Testing", () => {
+  it("can open a blog page", () => {
+    cy.visit("/");
 
-        cy.get('.post-entry:first').click();
-        
-        cy
-        .get('.post-title')
-        .should('be.visible')
-        .contains('Web scraping to create an api in 3 minutes!');
+    cy.get(".post-entry:first").click();
+
+    cy.get(".post-title")
+      .should("be.visible")
+      .contains("Web scraping to create an api in 3 minutes!");
+  });
+
+  it("visually check a post", () => {
+    cy.visit("/");
+
+    cy.get(".post-entry:first").click();
+
+    cy.get(".post-title").toMatchImageSnapshot({
+      imageConfig: { threshold: 0 },
     });
-
-    it('visually check a post', () => {
-        cy.visit('http://localhost:1313/');
-
-        cy.get('.post-entry:first').click();
-        
-        cy
-        .get('.post-title')
-        .toMatchImageSnapshot({imageConfig: {"threshold": 0}});
-    });
+  });
 });
